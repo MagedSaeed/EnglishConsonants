2450ec3666f71d25e88a214653e57bb43a96bfe5
MagedSaeed <mageedsaeed1@gmail.com>
2023-08-24 17:47:22 +0300

    add functionality to chunkify documents

--- english_consonants/experiments/language_modelling/src/datasets.py
+++ english_consonants/experiments/language_modelling/src/datasets.py
@@ -3,6 +3,15 @@ from tqdm.auto import tqdm
 from torch.utils.data import Dataset
 
 
+def chunkify_document(document, chunk_size):
+    # https://stackoverflow.com/a/312464/4412324
+    """construct a successive n-sized chunks from a document."""
+    chunks = list()
+    for i in range(0, len(document), chunk_size):
+        chunks.append(document[i : i + chunk_size])
+    return chunks
+
+
 class LanguageModelDataset(Dataset):
     def __init__(
         self,
@@ -27,6 +36,21 @@ class LanguageModelDataset(Dataset):
                 encoded_document.append(tokenizer.token_to_id(token))
             encoded_document.append(tokenizer.token_to_id("<eos>"))
             self.encoded_dataset.append(encoded_document)
+        # for document in dataset:
+        #     if not document:
+        #         continue
+        #     tokenized_document = tokenizer.tokenize_from_splits(document)
+        #     document_chunks = chunkify_document(
+        #         tokenized_document, chunk_size=sequence_length
+        #     )
+        #     for chunk in document_chunks:
+        #         if len(chunk) < (sequence_length - 2):
+        #             chunk += [tokenizer.pad_token] * (sequence_length - 2 - len(chunk))
+        #         encoded_chunk = [tokenizer.token_to_id("<bos>")]
+        #         for token in chunk[: sequence_length - 2]:
+        #             encoded_chunk.append(tokenizer.token_to_id(token))
+        #         encoded_chunk.append(tokenizer.token_to_id("<eos>"))
+        #         self.encoded_dataset.append(encoded_chunk)
 
     def __getitem__(self, index):
         inputs = self.encoded_dataset[index][:-1]
